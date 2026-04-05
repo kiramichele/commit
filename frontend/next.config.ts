d060349bb0d01bf76aa8aa4a5c328d652f3bce4b
kiramichele <106820715+kiramichele@users.noreply.github.com>
2026-04-05 19:17:56 -0400

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => config,
+  webpack: (config) => {
+    config.cache = false  // reduces memory usage during build
+    return config
+  },
 };
 
 export default nextConfig;
