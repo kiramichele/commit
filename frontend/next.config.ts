d9f75a6919574601c1acd518b1210f2b8bc34cf7
kiramichele <106820715+kiramichele@users.noreply.github.com>
2026-04-07 00:54:38 -0400

    simplify next config for vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.cache = false  // reduces memory usage during build
-    return config
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
